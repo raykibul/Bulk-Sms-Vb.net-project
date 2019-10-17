dddfd67f5d5856e83be0e129bb7cdd22d20ffac6
rakib <rakibcuet16@gmail.coim>
2019-10-18 00:33:06 +0600

    initilal

--- Bulk SMS/Form1.Designer.vb
+++ Bulk SMS/Form1.Designer.vb
@@ -22,51 +22,107 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.myport = New System.IO.Ports.SerialPort(Me.components)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.selectbtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.myworker = New System.ComponentModel.BackgroundWorker()
+        Me.statetext = New System.Windows.Forms.Label()
+        Me.total = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 14)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(158, 20)
-        Me.TextBox1.TabIndex = 0
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 40)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(158, 95)
-        Me.TextBox2.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 150)
+        Me.Button1.Location = New System.Drawing.Point(12, 371)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(158, 38)
+        Me.Button1.Size = New System.Drawing.Size(422, 38)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Send"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 23)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(175, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(22, 234)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(299, 131)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'selectbtn
+        '
+        Me.selectbtn.Location = New System.Drawing.Point(209, 21)
+        Me.selectbtn.Name = "selectbtn"
+        Me.selectbtn.Size = New System.Drawing.Size(75, 23)
+        Me.selectbtn.TabIndex = 5
+        Me.selectbtn.Text = "Select"
+        Me.selectbtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(454, 162)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = ""
+        '
+        'myworker
+        '
+        '
+        'statetext
+        '
+        Me.statetext.AutoSize = True
+        Me.statetext.Location = New System.Drawing.Point(522, 70)
+        Me.statetext.Name = "statetext"
+        Me.statetext.Size = New System.Drawing.Size(39, 13)
+        Me.statetext.TabIndex = 7
+        Me.statetext.Text = "Label1"
+        '
+        'total
+        '
+        Me.total.AutoSize = True
+        Me.total.Location = New System.Drawing.Point(512, 100)
+        Me.total.Name = "total"
+        Me.total.Size = New System.Drawing.Size(62, 13)
+        Me.total.TabIndex = 8
+        Me.total.Text = "Total Sent: "
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(745, 473)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.total)
+        Me.Controls.Add(Me.statetext)
         Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.selectbtn)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents myport As IO.Ports.SerialPort
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents selectbtn As Button
+    Friend WithEvents TextBox1 As RichTextBox
+    Friend WithEvents myworker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents statetext As Label
+    Friend WithEvents total As Label
 End Class
--- Bulk SMS/Form1.vb
+++ Bulk SMS/Form1.vb
@@ -1,3 +1,122 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+
+
+Public Class Form1
+
+    Dim counter As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            Dim ports As String() = SerialPort.GetPortNames
+            Dim port As String
+            For Each port In ports
+                ComboBox1.Items.Add(port)
+            Next port
+            ComboBox1.SelectedIndex = 0
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub selectbtn_Click(sender As Object, e As EventArgs) Handles selectbtn.Click
+        Try
+            With myport
+                .PortName = ComboBox1.Text
+                .BaudRate = 115200
+                .Parity = Parity.None
+                .StopBits = StopBits.One
+                .DataBits = 8
+                .Handshake = Handshake.RequestToSend
+                .DtrEnable = True
+                .RtsEnable = True
+                .NewLine = vbCrLf
+                .Open()
+            End With
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+
+    End Sub
+    Private Sub sendmsg(number As String)
+        Try
+            If myport.IsOpen Then
+
+                With myport
+                    .Write("AT" & vbCrLf)
+                    Thread.Sleep(500)
+                    .Write("AT+CMGF=1" & vbCrLf)
+                    Thread.Sleep(600)
+                    .Write("AT+CMGS=" & Chr(34) & number & Chr(34) & vbCrLf)
+                    Thread.Sleep(600)
+
+                    .Write(RichTextBox1.Text & "Sms No: " & counter & "now Time:" & Now.ToLongTimeString & Chr(26))
+
+                    counter += 1
+                    total.Text = " TOTAL SENT :" & counter
+
+                    Debug.WriteLine("SMS Sent TO : " & number)
+                    Debug.WriteLine("Current Time: ")
+                    Debug.WriteLine(Now.ToLongTimeString)
+
+
+                End With
+            Else
+                MsgBox("Error on the port selected")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        counter = 0
+
+        If myworker.IsBusy Then
+            statetext.Text = "NETWORK SLOW"
+            statetext.ForeColor = Color.IndianRed
+
+        Else
+
+
+            myworker.RunWorkerAsync()
+            statetext.Text = "NETWORK OKAY"
+            statetext.ForeColor = Color.Green
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub myworker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles myworker.DoWork
+        Control.CheckForIllegalCrossThreadCalls = False
+
+        Dim num As String
+        num = TextBox1.Text
+        Dim numbers As String() = num.Split(New Char() {","c})
+        Dim number As String
+        For Each number In numbers
+            Thread.Sleep(300)
+            Debug.WriteLine(number)
+            sendmsg(number)
+
+        Next
+        Debug.WriteLine("LOOP ENDS")
+
+
+    End Sub
+
 
 End Class
